c8658fe792ad988c03f3474111b498eae7811f1b
Adriano Maciel <adriano_mail@hotmail.com>
2022-12-04 11:35:41 -0300

    Criação automática das tabelas no banco de testes

---
+++ sql/init-database.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Delay, aguardar SQL Server..."
+
+sleep 30
+
+echo "Executando scripts..."
+
+cat /tmp/01-base-starwars.sql
+
+/opt/mssql-tools/bin/sqlcmd -S "${DB_HOST}" -U "${DB_USER}" -P "${DB_PASS}" -d "${DB_BASE}" -i /tmp/01-base-starwars.sql
